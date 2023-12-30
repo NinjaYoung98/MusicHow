5b80163838eb136d4b32efa6f4aab262d76c89e5
ChoChanYoung <whcksdud8@gmail.com>
2023-12-30 16:04:39 +0900

    env : init environment
    -프로젝트 초기 환경 세팅
    - spring boot 3.2.1
    - maria db 10.11.6
    - ignore yaml file

---
+++ src/test/java/com/musichow/musichow/MusicHowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musichow.musichow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicHowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
