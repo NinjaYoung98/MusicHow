8fb8594cefc255cc510791de4e1d633dced4b9f0
ChoChanYoung <whcksdud8@gmail.com>
2024-01-13 16:16:32 +0900

    env : 초기 환경 구성
    - spring boot 3.1.7
    - spring data jpa
    - mariadb 10.11.6

---
+++ src/main/java/com/musichow/MusicHowApplication.java
@@ -0,0 +1,13 @@
+package com.musichow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicHowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicHowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musichow/MusicHowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musichow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicHowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
